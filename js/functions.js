2aabf3427bb590ee72a68f65421d1e5cee6868a7
Alan Fung <wailunfung@gmail.com>
2016-04-18 21:40:57 -0700

    saving layout changes

--- js/functions.js
+++ js/functions.js
@@ -20,3 +20,21 @@ $(function(){
 		});
 	});
 });
+
+/**
+ * Listen to scroll to change header opacity class
+ */
+function checkScroll(){
+  var startY = $('.navbar-inverse').height() * 2; //The point where the navbar changes in px
+  if($(window).scrollTop() > startY){
+    $('.navbar-inverse').addClass("scrolled");
+  }else{
+    $('.navbar-inverse').removeClass("scrolled");
+  }
+}
+
+if($('.navbar').length > 0){
+  $(window).on("scroll load resize", function(){
+    checkScroll();
+  });
+}
